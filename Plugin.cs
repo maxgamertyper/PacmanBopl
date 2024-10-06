735c703a6513903dc2fae73996324ba4f80bd899
Maxgamertyper1 <99518006+maxgamertyper@users.noreply.github.com>
2024-10-06 16:22:06 -0600

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,4 @@
-﻿using BepInEx;
+using BepInEx;
 using BoplFixedMath;
 using HarmonyLib;
 using Microsoft.SqlServer.Server;
@@ -14,7 +14,7 @@ using static UnityEngine.ParticleSystem.PlaybackState;
 
 namespace Pacman
 {
-    [BepInPlugin("com.maxgamertyper1.pacmanborder", "Pacman Border", "1.0.0")]
+    [BepInPlugin("com.maxgamertyper1.pacmanborder", "Pacman Border", "1.1.0")]
     public class Suffocated : BaseUnityPlugin
     {
         private void Log(string message)
@@ -39,6 +39,8 @@ namespace Pacman
             Patch(harmony, typeof(ShootScaleChange), "Shoot", "ScaleChangePatch", true);
             Patch(harmony, typeof(RopeHook), "UpdateSim", "RopePatch", true);
             Patch(harmony, typeof(ShootQuantum), "Shoot", "BlinkPatch", true);
+            Patch(harmony, typeof(ShootDuplicator), "Shoot", "DuplicatorPatch", true);
+            Patch(harmony, typeof(BlackHole), "Update", "BlackHolePatch", true);
         }
 
         private void OnDestroy()
@@ -64,9 +66,101 @@ namespace Pacman
 
     public class Patches
     {
+        public static bool DuplicatorPatch(ref ShootDuplicator __instance, Vec2 firepointFIX, Vec2 directionFIX, ref bool hasFired, int playerId, bool alreadyHitWater = false)
+        {
+            if (__instance.maxDistance > 200)
+            {
+                __instance.maxDistance = (Fix)100L;
+            }
+            LayerMask mask = __instance.collisionMask;
+            if (alreadyHitWater)
+            {
+                mask = __instance.collisionMaskNoWater;
+            }
+            RaycastInformation raycastInformation = DetPhysics.Get().PointCheckAllRoundedRects(firepointFIX);
+            if (!raycastInformation)
+            {
+                raycastInformation = DetPhysics.Get().RaycastToClosest(firepointFIX, directionFIX, __instance.maxDistance, mask);
+            }
+            try
+            {
+                GameObject hit = raycastInformation.pp.fixTrans.gameObject;
+                return true;
+            }
+            catch (Exception e)
+            {
+                Vec2 RayEnd = firepointFIX + new Vec2(directionFIX.x * __instance.maxDistance, directionFIX.y * __instance.maxDistance);
+                Fix oldmax = __instance.maxDistance;
+                if (RayEnd.x < SceneBounds.BlastZone_XMin || RayEnd.x > SceneBounds.BlastZone_XMax)
+                {
+                    AudioManager.Get().Play("laserShoot");
+                    Vec2 NewPos;
+                    Fix OldLength;
+                    Fix NewLength;
+
+                    if (RayEnd.x < SceneBounds.BlastZone_XMin)
+                    {
+                        NewLength = (Fix)Math.Abs((long)(RayEnd.x - SceneBounds.BlastZone_XMin));
+                        OldLength = __instance.maxDistance - NewLength;
+                        NewPos = new Vec2(SceneBounds.BlastZone_XMax - (Fix)1, RayEnd.y);
+
+                    }
+                    else
+                    {
+                        NewLength = RayEnd.x - SceneBounds.BlastZone_XMax;
+                        OldLength = __instance.maxDistance - NewLength;
+                        NewPos = new Vec2(SceneBounds.BlastZone_XMin + (Fix)1, RayEnd.y);
+
+                    }
+                    Vec2 NewEnd = NewPos + new Vec2(directionFIX.x * NewLength, directionFIX.y * NewLength);
+
+                    __instance.maxDistance = NewLength;
+                    __instance.Shoot(NewPos, directionFIX, ref hasFired, playerId, alreadyHitWater);
+                    __instance.maxDistance = oldmax;
+
+                    ParticleSystem body = UnityEngine.Object.Instantiate<ParticleSystem>(__instance.rayParticle);
+
+                    ParticleSystem.ShapeModule shape = body.shape;
+                    ParticleSystem.EmissionModule emission = body.emission;
+
+                    ParticleSystem.Burst burst = emission.GetBurst(0);
+                    shape.radius = ((float)NewLength + __instance.trailEffectOffset) * 0.5f;
+                    body.transform.position = new Vector3((float)(NewPos.x + directionFIX.x * NewLength / (Fix)2f), (float)(NewPos.y + directionFIX.y * NewLength / (Fix)2f), 0.0f);
+                    body.transform.right = (Vector2)directionFIX;
+                    burst.count = Mathf.Max((float)NewLength * 0.5f, 2f);
+                    emission.SetBurst(0, burst);
+                    __instance.onHitParticle.transform.position = (Vector2)NewEnd;
+                    body.Simulate(0.11f);
+                    body.Play();
+                    __instance.onHitParticle.Play();
+
+                    __instance.spawnRayCastEffect((Vector2)firepointFIX, (Vector2)directionFIX, (float)__instance.maxDistance, false, false);
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public static void BlackHolePatch(ref BlackHole __instance)
+        {
+            GameObject parent = __instance.gameObject;
+            FixTransform parentft = parent.GetComponent<FixTransform>();
+            Vec2 parentpos = parentft.position;
+            if (parentpos.x >= SceneBounds.BlastZone_XMax)
+            {
+                Vector3 newPos = new Vector3((float)SceneBounds.BlastZone_XMin + 1f, (float)parentpos.y);
+                parentft.position = (Vec2)newPos;
+            }
+            else if (parentpos.x <= SceneBounds.BlastZone_XMin)
+            {
+                Vector3 newPos = new Vector3((float)SceneBounds.BlastZone_XMax - 1f, (float)parentpos.y);
+                parentft.position = (Vec2)newPos;
+            }
+        }
+
         public static bool ProjectilePatch(ref DestroyIfOutsideSceneBounds __instance, ref Fix simDeltaTime)
         {
-            FixTransform fixTrans = (FixTransform)Traverse.Create(__instance).Field("fixTrans").GetValue();
+            FixTransform fixTrans = __instance.fixTrans;
 
             if (__instance.DestroyIfTooFarX && (fixTrans.position.x < SceneBounds.BlastZone_XMin || fixTrans.position.x > SceneBounds.BlastZone_XMax))
             {
@@ -285,6 +379,4 @@ namespace Pacman
             }
         }
     }
-    // not working: beam
-    // fix the platforms that move out of bounce, i.e yellow things, asteroids
 }
